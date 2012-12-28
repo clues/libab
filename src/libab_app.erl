2f093eb8a25bd14e0b37f0a0878bd4c594a125bf
caolinong <linong.cao@csst.com>
2012-12-28 16:19:13 +0800

    init libab

---
+++ src/libab_app.erl
@@ -0,0 +1,16 @@
+-module(libab_app).
+
+-behaviour(application).
+
+%% Application callbacks
+-export([start/2, stop/1]).
+
+%% ===================================================================
+%% Application callbacks
+%% ===================================================================
+
+start(_StartType, _StartArgs) ->
+    libab_sup:start_link().
+
+stop(_State) ->
+    ok.
